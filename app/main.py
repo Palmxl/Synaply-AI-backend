4e1142f59d5e2844b77bc45643375d751ee0b585
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-05-17 19:43:50 -0500

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://synaply-ai-frontend.vercel.app/",
+        "https://synaply-ai-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
